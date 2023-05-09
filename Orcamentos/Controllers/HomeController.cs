57eec0ba694941698275feaafa3a0b321984e1ee
Hugo Gomes <2005hugogomes@gmail.com>
2023-05-09 10:03:57 +0100

    simple cards

--- Orcamentos/Controllers/HomeController.cs
+++ Orcamentos/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
+using Orcamentos.Infrastructure;
 using Orcamentos.Models;
 using System.Diagnostics;
 
@@ -6,15 +8,30 @@ namespace Orcamentos.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ILogger<HomeController> _logger;
+        private readonly DataContext _context;
+        private readonly IToastNotification _toastNotification;
+
+        public HomeController(ILogger<HomeController> logger, DataContext context, IToastNotification toastNotification)
         {
             _logger = logger;
+            _context = context;
+            _toastNotification = toastNotification;
         }
+
 
         public IActionResult Index()
         {
+            int totalOrcamentos = _context.orcamentos.Count();
+            decimal mediaPrecos = (decimal)_context.orcamentos.Average(o => o.TotalPrice);
+            decimal mediaCustos = (decimal)_context.orcamentos.Average(o => o.TotalCost);
+
+
+            ViewBag.TotalOrcamentos = totalOrcamentos;
+            ViewBag.MediaPrecos = mediaPrecos;
+            ViewBag.MediaCusto = mediaCustos;
+
             return View();
         }
 
